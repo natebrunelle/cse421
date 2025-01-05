d76758184a65b52f686ce9e79cd1435671a23453
Nathan Brunelle <njb2b@virginia.edu>
2025-01-05 12:43:13 -0800

    autocommit caused by update

--- cse421_wi25/update.sh
+++ cse421_wi25/update.sh
@@ -4,7 +4,7 @@ here="$(dirname "$(readlink -m "$0")")/"
 self="$(readlink -m "$0")"
 cd "$here"
 target="$(readlink -f "$here""/demo_site/")/"
-remote=brunelle@attu.cs.washington.edu:/cse/web/courses/cse421/25wi/demo
+remote=brunelle@attu.cs.washington.edu:/cse/web/courses/cse421/25wi
 if [ "$#" -gt 0 ] && [ "$1" != 'test' ]
 then
 	remote="$1"@attu.cs.washington.edu:/cse/web/courses/cse421/25wi
